6b31aaab55104a6a6c37a46154285e905ca05d39
am266 <am266@pc2-026-l.cs.st-andrews.ac.uk>
2014-03-31 16:30:12 +0100

    Adjusted consumer to run 10 connections to the same broker to make sure it worked as expected. 10 copies of each message are received, so it works

--- consumer.py
+++ consumer.py
@@ -13,6 +13,8 @@ import json
 import os
 import datetime
 
+brokers_list = []
+
 class SensorDataConsumerClientProtocol(WampClientProtocol):
    """
    Protocol for the sensor data consumer client. Uses WAMP protocol.
@@ -34,6 +36,7 @@ class SensorDataConsumerClientProtocol(WampClientProtocol):
       for channel in channels:
          print channel
          self.subscribe(channel, save_to_file)
+         # self.subscribe(channel, default_response)
 
       #self.subscribe("location", default_response)
       #self.subscribe("http://example.com/myEvent1", default_response) # hardwired for testing
@@ -45,6 +48,15 @@ def request_broker_address(registry_address,request_parameters):
    response = json.loads(response_json)
    return response["broker_address"]
 
+def request_brokers(registry_address, request_parameters):
+   payload = {'broker_param': request_parameters}
+   req = requests.get(registry_address + "/request_brokers", params=payload+"&maxbrokers="+max_brokers)
+   response_json = req.text
+   # TODO: parse JSON response
+      # if nothing valid, wait a while and then request again
+      # else, for each broker in json
+      #broker_list.append(broker)
+
 
 if __name__ == '__main__':
 
@@ -54,21 +66,33 @@ if __name__ == '__main__':
    parser.add_argument('-r', '--registry', type=str)
    parser.add_argument('-c', '--channels') #, nargs='+', type=str)
    parser.add_argument('-p', '--parameters', type=str)
+   parser.add_argument('-b', '--maxbrokers', type=str)
 
    args = parser.parse_args()
 
    channels = args.channels.split(",")
    parameters = args.parameters
    registry_address = args.registry
-
-   wsuri = request_broker_address(registry_address, parameters)
-
-   print "Connecting to", wsuri
+   max_brokers = args.maxbrokers
 
    ## our WAMP/WebSocket client
    ##
-   factory = WampClientFactory(wsuri, debugWamp = False)
-   factory.protocol = SensorDataConsumerClientProtocol
-   connectWS(factory)
+
+   ## TEST VERSION - makes 10 connections with one broker
+   wsuri = request_broker_address(registry_address, parameters)
+   print "Connecting to", wsuri
+   for i in range(0,10):
+      factory = WampClientFactory(wsuri, debugWamp = False)
+      factory.protocol = SensorDataConsumerClientProtocol
+      connectWS(factory) 
+   
+   # ## ACTUAL VERSION - loops through brokers in broker list, connecting to each
+   # broker_list = request_brokers(registry_address, parameters, max_brokers)
+   # for broker_address in broker_list:
+   #    wsuri = broker_address
+   #    print "Connecting to", wsuri
+   #    factory = WampClientFactory(wsuri, debugWamp = False)
+   #    factory.protocol = SensorDataConsumerClientProtocol
+   #    connectWS(factory)
    
    reactor.run()
