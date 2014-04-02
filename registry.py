d0d678599c5ffab3cabb41ca89cb409e82e8f543
asazonov <moherus@gmail.com>
2014-04-02 03:04:34 +0100

    Registry – replace broker

--- registry.py
+++ registry.py
@@ -43,8 +43,8 @@ def list_brokers():
     return resp
 
 
-@app.route("/request_new_broker", methods=['GET'])
-def request_new_broker():
+@app.route("/replace_broker", methods=['GET'])
+def replace_broker():
 """
 Used by the producer to request a new broker, if the initial one dies
 """
